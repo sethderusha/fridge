4d5ce72717e47b951e920ed3b20b40c900500db2
sethderusha <derushaseth@gmail.com>
2024-09-25 16:04:36 -0400

    Initial Commit

---
+++ fridge/fridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  fridgeApp.swift
+//  fridge
+//
+//  Created by Seth DeRusha on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct fridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ fridgeTests/fridgeTests.swift
@@ -0,0 +1,17 @@
+//
+//  fridgeTests.swift
+//  fridgeTests
+//
+//  Created by Seth DeRusha on 9/25/24.
+//
+
+import Testing
+@testable import fridge
+
+struct fridgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
